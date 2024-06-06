43e81f94b1fe87d757ac356ac448f58a52672a1a
Jeferson <jbasurco@unsa.edu.pe>
2024-06-06 01:05:24 -0500

    Agregando las rutas de turismo

--- Travello/telusko/telusko/urls.py
+++ Travello/telusko/telusko/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('travello.urls')),
     path('accounts/', include('accounts.urls')),
+    path('turismo/', include('turismo.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
