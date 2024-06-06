bd0ea908f66df532dc81019920aaf7cf8a0c5ce7
Jeferson <jbasurco@unsa.edu.pe>
2024-06-06 01:06:44 -0500

    Creando el modelo DestinoTuristico con sus atributos

--- Travello/telusko/turismo/models.py
+++ Travello/telusko/turismo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class DestinoTuristico(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='destinos/')
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    oferta = models.BooleanField(default=False)
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.nombre
